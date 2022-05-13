d189311dc5cc816e4e971badfd152342f04bed89
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-05-13 17:02:27 +0200

    Updates

--- source/models/cms/package.d
+++ source/models/cms/package.d
@@ -19,14 +19,7 @@ public import models.cms.entities;
 public import models.cms.tests;
 
 static this() {
-<<<<<<< HEAD
   EntityRegistry
     .register(CMSBlog, CMSDemo, CMSDocu, CMSGlossaryItem, 
       CMSLink, CMSNewsItem, CMSOffer, CMSPage, CMSTheme, CMSTutorial);
 }
-=======
-  uimEntityRegistry
-  .register(CMSBlog, CMSDemo, CMSDocu, CMSGlossaryItem, 
-    CMSLink, CMSNewsItem, CMSOffer, CMSPage, CMSTheme, CMSTutorial);
-}
->>>>>>> 146355525095d604c4252ad490da242b9b971cb9
